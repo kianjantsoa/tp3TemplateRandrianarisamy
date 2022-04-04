a047b5484b781ac5297414655e3cf9f53d5f02c7
jolivot <kianjantsoajolivot@gmail.com>
2022-04-04 14:31:36 +0300

    Ajout template JSF

---
+++ src/main/java/mg/itu/tp2randrianarisamy/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp2randrianarisamy.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author jolivot
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/mg/itu/tp2randrianarisamy/jsf/Beans.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tp2randrianarisamy.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author jolivot
+ */
+@Named(value = "beans")
+@RequestScoped
+public class Beans {
+
+    
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+    public String afficher() {
+        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+    /**
+     * Creates a new instance of Beans
+     */
+    
+    
+    public Beans() {
+    }
+    
+}
