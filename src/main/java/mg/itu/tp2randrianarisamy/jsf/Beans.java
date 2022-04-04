2325e13c72df43fd05db9bcdfb06f0b5d696161e
jolivot <kianjantsoajolivot@gmail.com>
2022-04-04 15:32:39 +0300

    Modification page

--- src/main/java/mg/itu/tp2randrianarisamy/jsf/Beans.java
+++ src/main/java/mg/itu/tp2randrianarisamy/jsf/Beans.java
@@ -37,7 +37,7 @@ public class Beans {
         return l;
     }
     public String afficher() {
-        return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
     /**
      * Creates a new instance of Beans
